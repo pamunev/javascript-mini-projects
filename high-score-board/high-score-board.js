0b983b5994c06b1015727044838888f7c9db36a5
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-01-29 17:32:28 -0600

    Completed first two functions of high-score-board.

--- high-score-board/high-score-board.js
+++ high-score-board/high-score-board.js
@@ -7,7 +7,8 @@
  * @returns {Record<string, number>} new score board
  */
 export function createScoreBoard() {
-  throw new Error('Please implement the createScoreBoard function');
+  const scoreBoard = { "The Best Ever": 1000000 };
+  return scoreBoard;
 }
 
 /**
@@ -19,7 +20,9 @@ export function createScoreBoard() {
  * @returns {Record<string, number>} updated score board
  */
 export function addPlayer(scoreBoard, player, score) {
-  throw new Error('Please implement the addPlayer function');
+  const newScoreBoard = scoreBoard;
+  newScoreBoard[player] = score;
+  return newScoreBoard;
 }
 
 /**
@@ -30,7 +33,7 @@ export function addPlayer(scoreBoard, player, score) {
  * @returns {Record<string, number>} updated score board
  */
 export function removePlayer(scoreBoard, player) {
-  throw new Error('Please implement the removePlayer function');
+  throw new Error("Please implement the removePlayer function");
 }
 
 /**
@@ -42,7 +45,7 @@ export function removePlayer(scoreBoard, player) {
  * @returns {Record<string, number>} updated score board
  */
 export function updateScore(scoreBoard, player, points) {
-  throw new Error('Please implement the updateScore function');
+  throw new Error("Please implement the updateScore function");
 }
 
 /**
@@ -52,7 +55,7 @@ export function updateScore(scoreBoard, player, points) {
  * @returns {Record<string, number>} updated score board
  */
 export function applyMondayBonus(scoreBoard) {
-  throw new Error('Please implement the applyMondayBonus function');
+  throw new Error("Please implement the applyMondayBonus function");
 }
 
 /**
@@ -62,5 +65,5 @@ export function applyMondayBonus(scoreBoard) {
  * @returns {number} normalized score
  */
 export function normalizeScore(params) {
-  throw new Error('Please implement the normalizeScore function');
+  throw new Error("Please implement the normalizeScore function");
 }
