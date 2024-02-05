e4936329e6be79c63b75db0a6507f419f14131d0
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-02-05 10:08:58 -0600

    finished first 2 functions of lucky-numbers. About to clean up code for second.

--- lucky-numbers/lucky-numbers.js
+++ lucky-numbers/lucky-numbers.js
@@ -8,7 +8,18 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Implement the twoSum function');
+  /*const string1 = String(array1);
+  console.log("string1:", string1);
+  const string2 = String(array2);*/
+  const string1 = array1.join("");
+  console.log("string1:", string1);
+  const string2 = array2.join("");
+  const number1 = Number(string1);
+  console.log("numb1:", number1);
+  const number2 = Number(string2);
+  console.log("num2:", number2);
+  const total = number1 + number2;
+  return total;
 }
 
 /**
@@ -18,7 +29,12 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Implement the luckyNumber function');
+  const numString = String(value);
+  const numArray = numString.split("");
+  const reversedArray = numArray.reverse();
+  const reversedString = reversedArray.join("");
+  const reversedNum = Number(reversedString);
+  return value === reversedNum;
 }
 
 /**
@@ -29,5 +45,5 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Implement the errorMessage function');
+  throw new Error("Implement the errorMessage function");
 }
