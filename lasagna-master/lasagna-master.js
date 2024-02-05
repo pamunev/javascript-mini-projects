f6e04be29aa57478dda1697086a03859758017c1
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-02-05 12:08:53 -0600

    Made some tests and console logs for 'quantities' function. Just want a record of that.

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,40 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime) {
+  if (remainingTime === 0) {
+    return `Lasagna is done.`;
+  }
+  if (!remainingTime) {
+    return `You forgot to set the timer.`;
+  }
+  if (remainingTime > 0) {
+    return `Not done, please wait.`;
+  }
+}
+
+export function preparationTime(layers, timePerLayer = 2) {
+  return layers.length * timePerLayer;
+}
+
+export function quantities(layers) {
+  let noodleLayers = 0;
+  let sauceLayers = 0;
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i] === "noodles") {
+      noodleLayers += 1;
+    }
+    if (layers[i] === "sauce") {
+      sauceLayers += 1;
+    }
+  }
+  let noodleGramsNeeded = noodleLayers * 50;
+  let sauceLitersNeeded = sauceLayers * 0.2;
+  console.log("noodLay:", noodleLayers);
+  console.log("noodgrams:", noodleGramsNeeded);
+  return { noodles: noodleGramsNeeded, sauce: sauceLitersNeeded };
+}
+
+const test = quantities(["noodles", "noodles", "sauce"]);
+console.log("test:", test);
