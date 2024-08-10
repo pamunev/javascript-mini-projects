b5bbf741a8b40d0d4ac7c3b25d5068e6d93c38ef
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-08-10 17:11:07 -0500

    Resize tests passing.

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -27,3 +27,19 @@ Position.prototype.move = function (newX, newY) {
   this.x = newX;
   this.y = newY;
 };
+
+export class ProgramWindow {
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
+  }
+
+  resize(newSize) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    this.size.width = Math.max(1, Math.min(maxWidth, newSize.width));
+    this.size.height = Math.max(1, Math.min(maxHeight, newSize.height));
+  }
+}
